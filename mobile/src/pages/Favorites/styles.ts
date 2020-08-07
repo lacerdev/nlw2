efb69310d210959b34becedd12382bdbed829ad9
André Lacerda da Silva <lasdeparq@gmail.com>
2020-08-07 19:24:26 -0300

    Aula 3 completa

---
+++ mobile/src/pages/Favorites/styles.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#f0f0f7',
+
+    }
+})
+
+export default styles;
---
+++ mobile/src/pages/TeacherList/styles.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f0f0f7',
+
+    }
+})
+
+export default style;
